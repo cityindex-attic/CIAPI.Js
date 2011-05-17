3431654205a37a7ba4acf350033e5f5dc7274769
David Laing <david@davidlaing.com>
2011-05-17 12:51:03 +0100

    Add skeleton for Chart widget
    Signed-off-by: David Laing <david@davidlaing.com>

---
+++ src/Chart/js/chart.js
@@ -0,0 +1,8 @@
+(function($, undefined) {
+
+
+    $(document).ready(function() {
+       CIAPI.log('initialising charts')
+    });
+
+})(jQuery);
---
+++ src/Chart/test/DoNothingTest.js
@@ -0,0 +1,8 @@
+describe("Placeholder test suite", function() {
+
+    it("should just pass", function() {
+        expect(1).toEqual(1);
+    });
+});
+
+
