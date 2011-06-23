6bd4015fae995b5e35c16767905a710bc1c6ee47
mrdavidlaing <david@davidlaing.com>
2011-06-23 16:08:38 +0100

    Ensure tests work with IE8

--- src/CIAPI/test/dto.PriceBar.UnitTests.js
+++ src/CIAPI/test/dto.PriceBar.UnitTests.js
@@ -1,25 +1,35 @@
-describe("PriceBar dto unit tests", function() {
+describe("PriceBar dto unit tests", function () {
     var date, priceBar;
+    var objectsCanBeFrozen = function () {
+        var anObject = { prop: "initial" };
+        Object.freeze(anObject);
+        anObject.prop = "canChange";
+        return anObject === "initial";
+    };
 
-    beforeEach(function() {
+    beforeEach(function () {
         date = new Date();
         priceBar = new CIAPI.dto.PriceBarDTO(date, 1, 2, 0, 1.5);
     });
 
-    it("should have readonly properties", function() {
-        expect(priceBar.BarDate).toEqual(date);
+    it("should have readonly properties", function () {
 
-        //Trying to set a property should do nothing
-        priceBar.BarDate = "foobar";
-        expect(priceBar.BarDate).toEqual(date);
+        if (objectsCanBeFrozen()) {
 
-        priceBar.Close = -1;
-        console.log(priceBar);
-        expect(priceBar.Close).toEqual(1.5);
+            expect(priceBar.BarDate).toEqual(date);
+
+            //Trying to set a property should do nothing
+            priceBar.BarDate = "foobar";
+            expect(priceBar.BarDate).toEqual(date);
+
+            priceBar.Close = -1;
+            console.log(priceBar);
+            expect(priceBar.Close).toEqual(1.5);
+        }
     });
 
-    it("should serialise to JSON correctly", function() {
-        var expectedJSON = '{"BarDate":'+JSON.stringify(date)+',"Open":1,"High":2,"Low":0,"Close":1.5}';
+    it("should serialise to JSON correctly", function () {
+        var expectedJSON = '{"BarDate":' + JSON.stringify(date) + ',"Open":1,"High":2,"Low":0,"Close":1.5}';
         expect(JSON.stringify(priceBar)).toEqual(expectedJSON);
     });
 
