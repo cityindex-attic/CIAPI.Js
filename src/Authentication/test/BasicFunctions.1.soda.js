4fdd0e748806fdc17069bad02411df26f35ce3c2
David Laing <david@davidlaing.com>
2011-08-05 16:05:54 +0100

    Some experimental Selenium tests

---
+++ src/Authentication/test/BasicFunctions.1.soda.js
@@ -0,0 +1,24 @@
+var soda = require('soda')
+        , assert = require('assert');
+
+var browser = soda.createClient({
+            host: 'localhost'
+            , port: 4444
+            , url: 'http://www.google.com'
+            , browser: 'firefox'
+        });
+
+browser
+        .chain
+        .session()
+        .open('/')
+        .type('q', 'Hello World')
+        .clickAndWait('btnG')
+        .getTitle(function(title) {
+            assert.ok(~title.indexOf('Hello World'))
+        })
+        .testComplete()
+        .end(function(err) {
+            if (err) throw err;
+            console.log('done');
+        });
